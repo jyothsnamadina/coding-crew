5ffd2879b5b62f84888fdd4a19681e5fde7b0be2
jyothsnamadina <137820069+jyothsnamadina@users.noreply.github.com>
2024-05-09 22:34:33 +0530

    Print Sum of series

---
+++ Sum of series.py
@@ -0,0 +1,17 @@
+//Write a program to find the sum of the given series 1+2+3+ . . . . . .(N terms) 
+class Solution:
+    def seriesSum(self, n : int) -> int:
+       
+         return n*(n+1)//2    // Here using the direct formula that sum of n natural numbers.
+
+if _name_ == '_main_':
+    t = int(input())
+    for _ in range(t):
+
+        n = int(input())
+
+        obj = Solution()
+        res = obj.seriesSum(n)
+
+        print(res)
+
