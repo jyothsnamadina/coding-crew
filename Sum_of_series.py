ed128fe49b01fec92d1df434f1246e0420182fc7
jyothsnamadina <137820069+jyothsnamadina@users.noreply.github.com>
2024-05-10 00:33:42 +0530

    print Sum of series

--- Sum_of_series.py
+++ Sum_of_series.py
@@ -1,8 +1,8 @@
-//Write a program to find the sum of the given series 1+2+3+ . . . . . .(N terms) 
+#Write a program to find the sum of the given series 1+2+3+ . . . . . .(N terms) 
 class Solution:
     def seriesSum(self, n : int) -> int:
        
-         return n*(n+1)//2    // Here using the direct formula that sum of n natural numbers.
+         return n*(n+1)//2    # Here using the direct formula that sum of n natural numbers.
 
 if _name_ == '_main_':
     t = int(input())
