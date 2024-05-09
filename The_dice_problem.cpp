78cfef02ae0d96fa375fa975990c50b953b5d174
jyothsnamadina <137820069+jyothsnamadina@users.noreply.github.com>
2024-05-09 23:33:42 +0530

    find opposite side of dice

---
+++ The_dice_problem.cpp
@@ -0,0 +1,25 @@
+//Find opposite face of dice
+#include<bits/stdc++.h> 
+using namespace std; 
+class Solution
+{
+public:
+    int oppositeFaceOfDice(int N)
+    {
+        return 7-N; //sum of opposite sides of dice is 7 .so if you want one side subract with another side
+    }
+};
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        int ans  = ob.oppositeFaceOfDice(N);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
